9b0685c0829405289d0a7da7c9ba51f08651176f
raloliver <contato@raloliver.com.br>
2022-12-12 04:56:11 +0000

    add socket.io on server and on client

---
+++ public/document.js
@@ -0,0 +1,9 @@
+/*
+ * File: document.js
+ * Project: websocket-nodejs-mongodb
+ * Created: Monday, December 12th 2022, 4:53:39 am
+ * Last Modified: Monday, December 12th 2022, 4:54:45 am
+ * Copyright © 2022 AMDE Agência
+ */
+
+const socket = io();
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
  * File: server.js
  * Project: websocket-nodejs-mongodb
  * Created: Monday, December 12th 2022, 4:34:29 am
- * Last Modified: Monday, December 12th 2022, 4:48:16 am
+ * Last Modified: Monday, December 12th 2022, 4:50:47 am
  * Copyright © 2022 AMDE Agência
  */
 
@@ -10,12 +10,18 @@ import express from 'express';
 import url from 'url';
 import path from 'path';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = process.env.port || 3000;
 const currentPath = url.fileURLToPath(import.meta.url);
 const publicPath = path.join(currentPath, '../..', 'public');
 const httpServer = http.createServer(app);
+const socket = new Server(httpServer);
 
 app.use(express.static(publicPath));
 httpServer.listen(port, () => console.info(`Server init on ${port}`));
+
+socket.on('connection', () => {
+    console.info('A new client has connected')
+});
